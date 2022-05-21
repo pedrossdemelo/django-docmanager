5b3df9c4dfca15ed79edaf68709a65ab6a355e2d
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-05-21 13:12:38 -0300

    fix: document id query
    int -> str (id is in uuid4 format)

--- document/urls.py
+++ document/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("docs/", views.document_list_view),
-    path("docs/<int:id>/", views.document_detail_view),
+    path("docs/<str:id>/", views.document_detail_view),
     path("companies/", views.company_list_view),
     path("companies/<str:name>/", views.company_detail_view),
     path("companies/<str:name>/docs/", views.company_documents_list_view),

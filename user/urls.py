b2bdcb0b760fb77e5d77b4b5a8b7cede3b9ad8ff
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-05-21 12:49:11 -0300

    feat: create user documents view

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.user_list_view),
     path("<str:email>/", views.user_detail_view),
     path("id/<int:id>/", views.user_detail_view),
+    path("<str:email>/docs", views.user_documents_view),
+    path("id/<int:id>/docs", views.user_documents_view),
 ]

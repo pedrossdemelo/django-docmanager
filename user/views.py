e5f48b159b5d3af82045d516896d6a739d491b3b
Pedro Sousa <83181309+pedrossdemelo@users.noreply.github.com>
2022-05-21 12:49:25 -0300

    feat: create user documents view

--- user/views.py
+++ user/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from document.models import Doc
+
+from document.serializers import DocumentSerializer
 from .models import User
 from .serializers import UserSerializer
 
@@ -22,8 +25,22 @@ class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
                 filter[field] = self.kwargs[field]
 
         obj = get_object_or_404(queryset, **filter)
-        self.check_object_permissions(self.request, obj)
         return obj
 
+
+class UserDocumentsView(generics.ListAPIView):
+    serializer_class = DocumentSerializer
+    multiple_lookup_fields = ("email", "id")
+
+    def get_queryset(self):
+        filter = {}
+        for field in self.multiple_lookup_fields:
+            if field in self.kwargs:
+                related_field = f"created_by__{field}"
+                filter[related_field] = self.kwargs[field]
+
+        return Doc.objects.filter(**filter, deleted=False)
+
+user_documents_view = UserDocumentsView.as_view()
 user_list_view = UserView.as_view()
 user_detail_view = UserDetailView.as_view()
